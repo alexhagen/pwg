3079b7f0aaf50cadc06c51444a736340d0f6ed8f
Alex Hagen <alexander.hagen@pnnl.gov>
2021-04-02 08:15:33 -0700

    uploaded to pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
-REQUIREMENTS = [
-                "matplotlib",
-                "numpy"
-                ]
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+
+REQUIREMENTS = ["matplotlib",
+                "numpy"]
+
+setup(name='pwg',
+      version=0.1,
+      description='Publication Worthy Graphics',
+      author='Alex Hagen',
+      author_email='alexhagen6@gmail.com',
+      url='https://github.com/alexhagen/pwg',
+      long_description=open('README.md').read(),
+      packages=['pwg'],
+      install_requires=REQUIREMENTS,
+     )
